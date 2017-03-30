fb192f821b2034c74b7d7751007e92cc2b95f22e
Max <lxghoney@163.com>
2017-03-30 17:45:35 +0800

    完善代码

--- src/main/java/com/lxg/acm/core/TestJudgeCore.java
+++ src/main/java/com/lxg/acm/core/TestJudgeCore.java
@@ -1,38 +1,40 @@
-//package com.lxg.acm.core;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//public class TestJudgeCore {
-//
-//	@Before
-//	public void init() {
-//	}
-//
-//	@Test
-//	public void testJava() {
-//		JudgeCore judge = new JudgeCore();
-//		judge.compileShell = "C:\\Program Files\\Java\\jdk1.7.0_75\\bin\\javac d:\\Main.java";
-//		judge.executeShell = "C:\\Program Files\\Java\\jdk1.7.0_75\\bin\\java -classpath d: Main";
-//		judge.dataForNum = "d:\\data\\1065";
-//		judge.run();
-//		System.out.println("result: " + judge.result);
-//		System.out.println("timeused: " + judge.timeUsed);
-//		System.out.println("memory: " + judge.memory);
-//		System.out.println("errorInfo: " + judge.errorInfo);
-//	}
-//
-//	@Test
-//	public void testCpp() {
-//		JudgeCore judge = new JudgeCore();
-//		judge.compileShell = "E:\\dev\\cfg\\MinGW\\bin\\gcc -o d:\\main d:\\main.c";
-//		judge.executeShell = "d:\\main.exe";
-//		judge.dataForNum = "d:\\data\\1065";
-//		judge.run();
-//		System.out.println("result: " + judge.result);
-//		System.out.println("timeused: " + judge.timeUsed);
-//		System.out.println("memory: " + judge.memory);
-//		System.out.println("errorInfo: " + judge.errorInfo);
-//	}
-//
-//}
+package com.lxg.acm.core;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/3/30.
+ */
+public class TestJudgeCore {
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void testJava() {
+        JudgeCore judge = new JudgeCore();
+        judge.compileShell = "D:\\acm\\test\\javac D:\\acm\\test\\Main.java";
+        judge.executeShell = "D:\\acm\\test\\java -classpath D:\\acm\\test\\Main";
+        judge.dataForNum = "D:\\acm\\test\\1";
+        judge.run();
+        System.out.println("result: " + judge.result);
+        System.out.println("timeused: " + judge.timeUsed);
+        System.out.println("memory: " + judge.memory);
+        System.out.println("errorInfo: " + judge.errorInfo);
+    }
+
+    @Test
+    public void testCpp() {
+        JudgeCore judge = new JudgeCore();
+        judge.compileShell = "D:\\acm\\test\\gcc -o D:\\acm\\test\\Hello D:\\acm\\test\\Hello.c";
+        judge.executeShell = "D:\\acm\\test\\Hello.exe";
+        judge.dataForNum = "D:\\acm\\test\\1";
+        judge.run();
+        System.out.println("result: " + judge.result);
+        System.out.println("timeused: " + judge.timeUsed);
+        System.out.println("memory: " + judge.memory);
+        System.out.println("errorInfo: " + judge.errorInfo);
+    }
+}
