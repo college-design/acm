ca7c7c2db8151b15caf799706d4a58890bfa09a0
Max <lxghoney@163.com>
2017-04-06 17:56:02 +0800

    添加用户名是否注册过接口

--- src/main/java/com/lxg/acm/controller/UserController.java
+++ src/main/java/com/lxg/acm/controller/UserController.java
@@ -3,8 +3,6 @@ package com.lxg.acm.controller;
 import com.lxg.acm.entity.User;
 import com.lxg.acm.exception.UserException;
 import com.lxg.acm.mapper.UserMapper;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,8 +19,6 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-	private static final Log logger = LogFactory.getLog(UserController.class);// 日志
-	
 	@Autowired
 	private UserMapper userMapper;
 
@@ -34,7 +30,6 @@ public class UserController {
 	@RequestMapping("/userAdd")
 	public String addUser(User user){
 //		user.setPassword(StringUtil.md5(user.getPassword(),"acm"));
-		logger.info("========UserController->addUser添加用户user={"+user+"}========");
 		Long result = userMapper.save(user);
 		return "redirect:/problemlist/1/50";
 	}
@@ -47,7 +42,6 @@ public class UserController {
 	 */
 	@RequestMapping("/userlist/{page}/{offset}")
 	public String findList(@PathVariable Long page, @PathVariable Long offset) {
-		logger.info("========UserController->用户分页列表信息========未完成");
 		return "userlist";
 	}
 	
@@ -72,7 +66,6 @@ public class UserController {
 			throw new UserException("用户不存在");
 		}
 		model.addAttribute("user", user);
-		logger.info("========UserController->登录用户信息={"+user.toString()+"}========");
 		return "userinfo";
 	}
 
@@ -83,7 +76,6 @@ public class UserController {
 	 */
 	@RequestMapping("/regist")
 	public String regist(User user) {
-		logger.info("========UserController->用户注册={"+user.toString()+"}========");
 		return "regist";
 	}
 
@@ -95,7 +87,6 @@ public class UserController {
 	 */
 	@RequestMapping(value = "/update/{uid}", method = RequestMethod.GET)
 	public String updatePage(@PathVariable Long uid, Model model) {
-		logger.info("========UserController->用户修改页面========");
 		User user = userMapper.query(uid);
 		model.addAttribute("user", user);
 		return "update";
@@ -108,7 +99,6 @@ public class UserController {
 	 */
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	public String update(User user) {
-		logger.info("========UserController->用户修改={"+user.toString()+"}========");
 		userMapper.update(user);
 		return "redirect:/problemlist/1/50";
 	}
@@ -116,14 +106,12 @@ public class UserController {
 	// 跳转登陆页面
 	@RequestMapping(value = "/login")
 	public String login(User user) {
-		logger.info("========UserController->用户登录={"+user.toString()+"}========");
 		return "login";
 	}
 
 	// 退出
 	@RequestMapping(value = "/logout")
 	public String logout() {
-		logger.info("========UserController->用户退出========");
 		return "index";
 	}
 
@@ -132,7 +120,6 @@ public class UserController {
 	@ResponseBody
 	public String remove(Long user) {
 //		userMapper.delete(user);
-		logger.info("========UserController->删除用户={"+user.toString()+"}========未完成");
 		return "";
 	}
 
